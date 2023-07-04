eea2ea6770ce1276f790f993c2e39ddf96913b34
Christopher Trott <chris@twocentstudios.com>
2023-07-04 18:46:56 +0900

    Initial Commit

---
+++ goalie/goalieApp.swift
@@ -0,0 +1,17 @@
+//
+//  goalieApp.swift
+//  goalie
+//
+//  Created by Christopher Trott on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct goalieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
