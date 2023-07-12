56834a535abb91e376dcd4d15aaed30e7f1be5a8
Christopher Trott <chris@twocentstudios.com>
2023-07-12 19:39:08 +0900

    weekDayIntervals

--- goalie/WeekView.swift
+++ goalie/WeekView.swift
@@ -14,11 +14,13 @@ final class WeekStore: ObservableObject {
         @Dependency(\.date.now) var now
         @Dependency(\.calendar) var calendar
         @Dependency(\.timeZone) var timeZone
+        
+        // TODO: should we use Gregorian Calendar for calendar?
 
         let week = Week(date: now, calendar: calendar)
         let topicWeek = TopicWeek(topic: topic, week: week)
         self.topicWeek = topicWeek
-        
+
         let dateFormatter = DateFormatter()
         dateFormatter.calendar = calendar
         dateFormatter.timeZone = timeZone
@@ -32,23 +34,28 @@ extension Week {
         let inputDateComponents = calendar.dateComponents([.yearForWeekOfYear, .weekOfYear, .month], from: date)
         self.init(inputDateComponents: inputDateComponents, calendar: calendar)
     }
-    
+
+    /// inputDateComponents should contain `.yearForWeekOfYear`, `.weekOfYear`, `.month`.
     init(inputDateComponents: DateComponents, calendar: Calendar) {
+        assert(inputDateComponents.yearForWeekOfYear != nil)
+        assert(inputDateComponents.weekOfYear != nil)
+        assert(inputDateComponents.month != nil)
+
         // Get the date components for the first day of the relevant week
         var firstDayOfWeekComponents = inputDateComponents
         firstDayOfWeekComponents.weekday = 1
         firstDayOfWeekComponents.calendar = calendar
         let firstDayOfWeekDate = firstDayOfWeekComponents.date!
-        
+
         // Use the date of the first day of the week to calculate the day
         firstDayOfWeekComponents = calendar.dateComponents([.yearForWeekOfYear, .weekOfYear, .month, .day], from: firstDayOfWeekDate)
-        
+
         // Get the date components for the last day of the relevant week
         var lastDayOfWeekComponents = inputDateComponents
         lastDayOfWeekComponents.weekday = 7
         lastDayOfWeekComponents.calendar = calendar
         let lastDayOfWeekDate = lastDayOfWeekComponents.date!
-        
+
         lastDayOfWeekComponents = calendar.dateComponents([.yearForWeekOfYear, .weekOfYear, .month, .day], from: lastDayOfWeekDate)
         // Use the date of the first day of the week to calculate the day
 
@@ -57,6 +64,25 @@ extension Week {
         month = inputDateComponents.month!
         firstDayOfWeek = firstDayOfWeekComponents.day!
         lastDayOfWeek = lastDayOfWeekComponents.day!
+    }
+
+    static func dayInterval(for inputDateComponents: DateComponents) -> DayInterval {
+        assert(inputDateComponents.yearForWeekOfYear != nil)
+        assert(inputDateComponents.weekOfYear != nil)
+        assert(inputDateComponents.month != nil)
+        assert(inputDateComponents.day != nil)
+        assert(inputDateComponents.calendar != nil)
+        let calendar = inputDateComponents.calendar!
+
+        let start = inputDateComponents.date!
+        assert(start == calendar.startOfDay(for: start))
+
+        let almostOneDayComponents = DateComponents(day: 1, second: -1)
+        guard let end = calendar.date(byAdding: almostOneDayComponents, to: start, wrappingComponents: true) else {
+            fatalError("Couldn't get endDate from startDate: \(start)")
+        }
+        
+        return DayInterval(startDate: start, endDate: end)
     }
     
     // Returns first day of week
@@ -69,23 +95,38 @@ extension Week {
         components.day = firstDayOfWeek
         return components
     }
-    
+
     func previousWeek(calendar: Calendar) -> Week? {
         let minusOneWeekDurationComponents = DateComponents(weekOfYear: -1)
         return weekByAddingDateComponents(durationComponents: minusOneWeekDurationComponents, calendar: calendar)
     }
-    
+
     func nextWeek(calendar: Calendar) -> Week? {
         let plusOneWeekDurationComponents = DateComponents(weekOfYear: 1)
         return weekByAddingDateComponents(durationComponents: plusOneWeekDurationComponents, calendar: calendar)
     }
-    
+
     func weekByAddingDateComponents(durationComponents: DateComponents, calendar: Calendar) -> Week? {
         let components = dateComponents
         guard let currentDate = calendar.date(from: components) else { return nil }
         guard let nextWeekDate = calendar.date(byAdding: durationComponents, to: currentDate, wrappingComponents: true) else { return nil }
         let nextWeek = Week(date: nextWeekDate, calendar: calendar)
         return nextWeek
+    }
+
+    /// Returns an array of 7 tuples corresponding to the 7 days of the week starting on Sunday
+    /// The first date is midnight, the second is the last second of the same day.
+    func weekDayIntervals(calendar: Calendar) -> (DayInterval, DayInterval, DayInterval, DayInterval, DayInterval, DayInterval, DayInterval) {
+        let startDateComponents = dateComponents
+        var dayIntervals: [DayInterval] = []
+        for weekday in 1...7 {
+            var weekdayComponents = startDateComponents
+            weekdayComponents.weekday = weekday
+            weekdayComponents.day = nil
+            let dayInterval = Self.dayInterval(for: weekdayComponents)
+            dayIntervals.append(dayInterval)
+        }
+        return (dayIntervals[0], dayIntervals[1], dayIntervals[2], dayIntervals[3], dayIntervals[4], dayIntervals[5], dayIntervals[6])
     }
 }
 
@@ -102,6 +143,11 @@ struct Week: Equatable, Identifiable {
     var month: Int
     var firstDayOfWeek: Int // 9 -> July 9th
     var lastDayOfWeek: Int
+}
+
+struct DayInterval: Equatable {
+    var startDate: Date // Midnight
+    var endDate: Date // Midnight of next day minus 1 minute
 }
 
 struct WeekViewData {
@@ -121,20 +167,20 @@ struct WeekViewData {
 
 struct WeekScreen: View {
     @ObservedObject var store: WeekStore
-    
+
     private var viewData: WeekViewData {
         let topicWeek = store.topicWeek
         let monthName = store.dateFormatter.standaloneMonthSymbols[topicWeek.week.month - 1]
         let viewData = WeekViewData(
             title: "Week \(topicWeek.week.weekOfYear)",
             subtitle: "\(monthName) \(topicWeek.week.firstDayOfWeek)-\(topicWeek.week.lastDayOfWeek), \(topicWeek.week.yearForWeekOfYear)",
-            previousWeekDisabled: false, // TODO
-            nextWeekDisabled: false, // TODO
+            previousWeekDisabled: false, // TODO:
+            nextWeekDisabled: false, // TODO:
             days: []
         )
         return viewData
     }
-    
+
     var body: some View {
         WeekView(viewData: viewData)
     }
