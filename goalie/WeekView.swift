540d5e17c5bcf7d9b46914d1a1217bf147699147
Christopher Trott <chris@twocentstudios.com>
2023-07-12 18:42:11 +0900

    Initial week store and calculations

--- goalie/WeekView.swift
+++ goalie/WeekView.swift
@@ -1,5 +1,66 @@
+import Dependencies
+import Foundation
 import SwiftUI
 import SystemColors
+
+final class WeekStore: ObservableObject {
+//    @Dependency(\.uuid) var uuid
+    // @Dependency(\.mainRunLoop) var mainRunLoop
+
+    @Published var topicWeek: TopicWeek
+    let dateFormatter: DateFormatter
+
+    init(topic: Topic) {
+        @Dependency(\.date.now) var now
+        @Dependency(\.calendar) var calendar
+        @Dependency(\.timeZone) var timeZone
+
+        // Get components for the relevant week
+        let nowComponents = calendar.dateComponents([.yearForWeekOfYear, .weekOfYear, .month], from: now)
+
+        // Get the date components for the first day of the relevant week
+        var firstDayOfWeekComponents = nowComponents
+        firstDayOfWeekComponents.weekday = 1
+        firstDayOfWeekComponents.calendar = calendar
+        let firstDayOfWeekDate = firstDayOfWeekComponents.date!
+        
+        // Use the date of the first day of the week to calculate the day
+        firstDayOfWeekComponents = calendar.dateComponents([.yearForWeekOfYear, .weekOfYear, .month, .day], from: firstDayOfWeekDate)
+        
+        // Get the date components for the last day of the relevant week
+        var lastDayOfWeekComponents = nowComponents
+        lastDayOfWeekComponents.weekday = 7
+        lastDayOfWeekComponents.calendar = calendar
+        let lastDayOfWeekDate = lastDayOfWeekComponents.date!
+        
+        lastDayOfWeekComponents = calendar.dateComponents([.yearForWeekOfYear, .weekOfYear, .month, .day], from: lastDayOfWeekDate)
+        // Use the date of the first day of the week to calculate the day
+
+        let week = Week(yearForWeekOfYear: nowComponents.yearForWeekOfYear!, weekOfYear: nowComponents.weekOfYear!, month: nowComponents.month!, firstDayOfWeek: firstDayOfWeekComponents.day!, lastDayOfWeek: lastDayOfWeekComponents.day!)
+        let topicWeek = TopicWeek(topic: topic, week: week)
+        self.topicWeek = topicWeek
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.calendar = calendar
+        dateFormatter.timeZone = timeZone
+        self.dateFormatter = dateFormatter
+    }
+}
+
+struct TopicWeek: Equatable, Identifiable {
+    var id: String { "\(topic.id):\(week.id)" }
+    let topic: Topic
+    let week: Week
+}
+
+struct Week: Equatable, Identifiable {
+    var id: String { "\(yearForWeekOfYear):\(weekOfYear)" } // 2023:32
+    var yearForWeekOfYear: Int
+    var weekOfYear: Int
+    var month: Int
+    var firstDayOfWeek: Int // 9 -> July 9th
+    var lastDayOfWeek: Int
+}
 
 struct WeekViewData {
     struct Day: Equatable, Identifiable {
@@ -17,8 +78,23 @@ struct WeekViewData {
 }
 
 struct WeekScreen: View {
+    @ObservedObject var store: WeekStore
+    
+    private var viewData: WeekViewData {
+        let topicWeek = store.topicWeek
+        let monthName = store.dateFormatter.standaloneMonthSymbols[topicWeek.week.month - 1]
+        let viewData = WeekViewData(
+            title: "Week \(topicWeek.week.weekOfYear)",
+            subtitle: "\(monthName) \(topicWeek.week.firstDayOfWeek)-\(topicWeek.week.lastDayOfWeek), \(topicWeek.week.yearForWeekOfYear)",
+            previousWeekDisabled: false, // TODO
+            nextWeekDisabled: false, // TODO
+            days: []
+        )
+        return viewData
+    }
+    
     var body: some View {
-        WeekView(viewData: .mock)
+        WeekView(viewData: viewData)
     }
 }
 
@@ -91,6 +167,8 @@ struct WeekView: View {
 struct WeekView_Previews: PreviewProvider {
     static var previews: some View {
         WeekView(viewData: .mock)
+            .frame(width: 200)
+        WeekScreen(store: .init(topic: .new))
             .frame(width: 200)
     }
 }
